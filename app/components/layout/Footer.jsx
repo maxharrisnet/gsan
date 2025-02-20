95018be87fe664e9bbb93589e3261609a73370fa
Max Harris <hello@maxharris.net>
2025-02-20 09:20:16 -0800

    removing footer bug

--- app/components/layout/Footer.jsx
+++ app/components/layout/Footer.jsx
@@ -1,11 +1,8 @@
-import React from 'react';
-// import './Footer.css';
-
 const Footer = () => {
 	return (
 		<footer className='footer'>
 			<div className='footer-container'>
-				<span>© 2025 GSAN - All rights reserved.</span>
+				<span>2025 GSAN - All rights reserved.</span>
 			</div>
 		</footer>
 	);
