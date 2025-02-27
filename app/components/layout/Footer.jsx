69e9e23655ebea6f5598989556c2f92b96f64ed2
maxharrisnet <hello@maxharris.codes>
2025-02-26 19:22:06 -0800

    fix hydration error

--- app/components/layout/Footer.jsx
+++ app/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
 	return (
 		<footer className='footer'>
 			<div className='footer-container'>
-				<span>© 2025 Switch - All rights reserved.</span>
+				<span>2025 Switch - All rights reserved.</span>
 			</div>
 		</footer>
 	);
