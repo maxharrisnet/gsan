90b05698b9bed3f3f28c8603153e825af485832b
Max Harris <hello@maxharris.net>
2025-03-05 13:16:55 -0800

    contact details

--- app/routes/maintenance.jsx
+++ app/routes/maintenance.jsx
@@ -7,24 +7,26 @@ export function links() {
 export default function Maintenance() {
 	return (
 		<div className='maintenance-container'>
+			<img
+				src='/assets/images/switch-logo.png'
+				alt='Switch Logo'
+				className='login-logo'
+			/>
 			<div className='maintenance-content'>
 				<span className='material-icons maintenance-icon'>build</span>
 				<h1>Under Maintenance</h1>
 				<p>We're currently performing system updates to improve your experience.</p>
-				<p className='maintenance-details'>
-					Expected completion: <strong>2 hours</strong>
-				</p>
+
 				<div className='maintenance-contact'>
 					<p>For urgent matters, please contact:</p>
 					<a
-						href='tel:+1234567890'
+						href='tel:+18776283801'
 						className='contact-link'
 					>
-						<span className='material-icons'>phone</span>
-						1-234-567-890
+						<span className='material-icons'>phone</span>+1 (877) 628-3801
 					</a>
 					<a
-						href='mailto:support@example.com'
+						href='info@switch.ca'
 						className='contact-link'
 					>
 						<span className='material-icons'>email</span>
